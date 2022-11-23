1a15d89ab7d4b545ba7d8b92b95a03111f0fea3a
Owais Dashti <owaisdashti@outlook.com>
2022-11-23 22:43:44 +0100

    making it ready for ecodriver project

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,55 +4,19 @@ import { useRef } from "react";
 import facade from "../facades/apiFacade";
 
 function Search({ movieData, setMovieData }) {
-  const inputRef = useRef();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    window.sessionStorage.setItem("movie", movieData);
-  }, [movieData]);
-
-  const handleMovieSearch = (event) => {
-    event.preventDefault();
-    if (inputRef.current.value === "") return;
-    setIsLoading(true);
-    facade
-      .searchMovie(inputRef.current.value)
-      .then((res) => {
-        setMovieData(res);
-        inputRef.current.value = "";
-      })
-      .catch((err) => {
-        //err.fullError.then(e => setErrorMsg(e.message))
-        //navigate("/error")
-      });
-    setIsLoading(false);
-  };
 
   return (
     <>
       <div>
-        <div class="nine">
+        <div className="nine">
           <h1>
-            Search <span style={{ color: "white" }}>Look up a movie by its title</span>{" "}
+            About Us <span style={{ color: "white" }}>Est. 2022</span>{" "}
           </h1>
         </div>
-        <div className="search-bar">
-          <form className="search-field-form">
-            <div className="search-field">
-              <input id="search" type="text" placeholder="Search..." ref={inputRef} required />
-            </div>
-            <button type="submit" onClick={handleMovieSearch}>
-              <i className="fa fa-fw fa-search"></i>
-            </button>
-          </form>
-        </div>
+
       </div>
 
-      {isLoading ? (
-        <h1>Loading movie...</h1>
-      ) : (
-        movieData !== undefined && <Movie movieData={movieData} />
-      )}
+
     </>
   );
 }
