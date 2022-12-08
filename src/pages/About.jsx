7ea3a5d36fbad810c6ea412419733fb082ea20cf
Owais Dashti <owaisdashti@outlook.com>
2022-12-08 19:21:41 +0100

    working on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaEnvelope, FaPhone } from "react-icons/fa";
 
 function About() {
   return (
@@ -9,6 +10,45 @@ function About() {
             About Us <span style={{ color: "white" }}>Est. 2022</span>{" "}
           </h1>
         </div>
+
+        <div className="about-container">
+          <div className="about-left">
+            <p>Vi svarer hurtigst muligt på:</p>
+            <p><FaEnvelope/> <a href = "mailto:support@ecodrivr.dk" style={{textDecoration: "none"}}>support@ecodrivr.dk</a></p>
+            <hr />
+            <p>Ring til os</p>
+            <p><FaPhone/> <a href="tel:+45 00 00 00 00">+45 00 00 00 00</a></p>
+            <hr />
+            <p>Åbningstid</p>
+            <p>Mandag-Fredag</p>
+            <p>10:00-12:00, 13-00-18:00</p>
+
+          </div>
+
+          <div className="about-right">
+            <div>
+              <img src="" alt="" />
+              <p>Andreas</p>
+            </div>
+
+            <div>
+              <img src="" alt="" />
+              <p>Daniel</p>
+            </div>
+
+            <div>
+              <img src="" alt="" />
+              <p>Owais</p>
+            </div>
+
+            <div>
+              <img src="" alt="" />
+              <p>Thomas</p>
+            </div>
+
+          </div>
+        </div>
+
       </div>
       <div className="overlay-about"></div>
     </>
