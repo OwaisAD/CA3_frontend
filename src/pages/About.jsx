be73dfc80be1cb282f9163f3bd68568413013b72
Owais Dashti <owaisdashti@outlook.com>
2022-12-08 19:24:05 +0100

    working on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaEnvelope, FaPhone } from "react-icons/fa";
+import { FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 
 function About() {
   return (
@@ -14,15 +14,21 @@ function About() {
         <div className="about-container">
           <div className="about-left">
             <p>Vi svarer hurtigst muligt på:</p>
-            <p><FaEnvelope/> <a href = "mailto:support@ecodrivr.dk" style={{textDecoration: "none"}}>support@ecodrivr.dk</a></p>
+            <p>
+              <FaEnvelope size={20}/>
+              <a href="mailto:support@ecodrivr.dk" style={{ textDecoration: "none" }}>
+                support@ecodrivr.dk
+              </a>
+            </p>
             <hr />
             <p>Ring til os</p>
-            <p><FaPhone/> <a href="tel:+45 00 00 00 00">+45 00 00 00 00</a></p>
+            <p>
+              <FaPhoneAlt size={20} /> <a href="tel:+45 00 00 00 00" style={{ textDecoration: "none" }}>+45 00 00 00 00</a>
+            </p>
             <hr />
             <p>Åbningstid</p>
             <p>Mandag-Fredag</p>
             <p>10:00-12:00, 13-00-18:00</p>
-
           </div>
 
           <div className="about-right">
@@ -45,10 +51,8 @@ function About() {
               <img src="" alt="" />
               <p>Thomas</p>
             </div>
-
           </div>
         </div>
-
       </div>
       <div className="overlay-about"></div>
     </>
